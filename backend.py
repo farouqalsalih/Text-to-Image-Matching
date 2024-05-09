ce329c47c6bccaac59fb86c4e2d66f29de98a7fb
Farouq Alsalih <farouqalsalih@gmail.com>
2024-05-09 02:00:56 -0400

    update

--- backend.py
+++ backend.py
@@ -1,3 +1,23 @@
 from sentence_transformers import SentenceTransformer, util
 from PIL import Image
 
+list = [
+"IMG_0008.jpg", 
+"IMG_0271.jpg",
+"IMG_0335.jpg",
+"IMG_0618.jpg",
+"IMG_0678.jpg",
+"IMG_0968.jpg",
+"IMG_2331.jpg",
+"IMG_3102.jpg",
+"IMG_3137.jpg",
+"IMG_3213.jpg"
+]
+
+model = SentenceTransformer('clip-ViT-B-32')
+
+image_list = []
+for image in list:
+    image_list.append(Image.open(image))
+
+
