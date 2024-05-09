7bd1bba06363cdc96c2e3791df78222e841ccab5
Fernando Bedoya <fbedoyao@dhcp-10-5-208-93.wireless.rochester.edu>
2024-05-09 02:21:02 -0400

    Add basic index and result pages

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    # Process the search query here (e.g., search in a database)
+    return f'Searching for: {query}'
 
 if __name__ == '__main__':
     app.run(debug=True)
