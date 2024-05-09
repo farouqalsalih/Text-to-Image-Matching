5cb981e1f5cea73a6287b7fe1bc557d966c1ceb9
Farouq Alsalih <farouqalsalih@gmail.com>
2024-05-09 02:15:04 -0400

    burrito

--- backend.py
+++ backend.py
@@ -2,16 +2,16 @@ from sentence_transformers import SentenceTransformer, util
 from PIL import Image
 
 list = [
-"IMG_0008.jpg", 
-"IMG_0271.jpg",
-"IMG_0335.jpg",
-"IMG_0618.jpg",
-"IMG_0678.jpg",
-"IMG_0968.jpg",
-"IMG_2331.jpg",
-"IMG_3102.jpg",
-"IMG_3137.jpg",
-"IMG_3213.jpg"
+    "images/IMG_0008.jpg", 
+    "images/IMG_0271.jpg",
+    "images/IMG_0335.jpg",
+    "images/IMG_0618.jpg",
+    "images/IMG_0678.jpg",
+    "images/IMG_0968.jpg",
+    "images/IMG_2331.jpg",
+    "images/IMG_3102.jpg",
+    "images/IMG_3137.jpg",
+    "images/IMG_3213.jpg"
 ]
 
 model = SentenceTransformer('clip-ViT-B-32')
@@ -21,3 +21,22 @@ for image in list:
     image_list.append(Image.open(image))
 
 
+image_emb = model.encode(image_list)
+
+print("Begin Testing:\n")
+
+while True:
+    user_input = input()
+    text_embedding = model.encode([user_input])
+    cos_sim = util.cos_sim(text_embedding, image_emb)[0]
+    print(cos_sim)
+
+    max = cos_sim[0]
+    index = 0
+    for idx, element in enumerate(cos_sim):
+        if element > max:
+            max = element
+            index = idx
+
+    img_to_show = Image.open(list[index])
+    img_to_show.show()
